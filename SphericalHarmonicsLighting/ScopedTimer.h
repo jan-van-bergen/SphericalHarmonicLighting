f75571f6af3043ca0e63d544a9e3e6d97486bd8e
jan <janvanbergen16@gmail.com>
2019-09-06 12:31:06 +0200

    ScopedTimer now reports its result in a more readable way

--- SphericalHarmonicsLighting/ScopedTimer.h
+++ SphericalHarmonicsLighting/ScopedTimer.h
@@ -17,6 +17,12 @@ public:
 		auto stop_time = std::chrono::high_resolution_clock::now();
 		u128 duration  = std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time).count();
 
-		printf("%s took: %llu us\n", name, duration);
+		if (duration >= 1000000) {
+			printf("%s took: %llu us (%llu s)\n", name, duration, duration / 1000000);
+		} else if (duration >= 1000) {
+			printf("%s took: %llu us (%llu ms)\n", name, duration, duration / 1000);
+		} else {
+			printf("%s took: %llu us\n", name, duration);
+		}
 	}
 };
