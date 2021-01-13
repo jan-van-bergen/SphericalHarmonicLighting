2b214b3451859ded07ea4e41fb2aa8d2a44ab579
jan <janvanbergen16@gmail.com>
2021-01-13 17:55:59 +0100

    MSAA

--- SphericalHarmonicsLighting/Display.cpp
+++ SphericalHarmonicsLighting/Display.cpp
@@ -13,6 +13,10 @@ Display::Display(u32 width, u32 height, const char* title) : width(width), heigh
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+	// MSAA
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 	window  = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
